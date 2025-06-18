5104e7990eca804f0d472797ade89f0fabea96d0
irene29-hub <muemarenee@gmail.com>
2025-06-18 23:05:30 +0300

    add changes

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
-const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
-
 // Code your solution here
+const batteryBatches = [
+  { batchNum: 1, amount: 4 },
+  { batchNum: 2, amount: 5 },
+  { batchNum: 3, amount: 3 },
+  { batchNum: 4, amount: 4 },
+  { batchNum: 5, amount: 4 },
+  { batchNum: 6, amount: 6 },
+  { batchNum: 7, amount: 5 },
+];
+
+const totalBatteries = batteryBatches.reduce((accumulator, batch) => {
+  return accumulator + batch.amount;
+}, 0); // Initialize accumulator with 0
+
+// You can test the result by logging it:
+// console.log(totalBatteries);
